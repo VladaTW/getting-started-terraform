ae5b59c29b01e0034532ce69c98419fb5b4633b8
VladaTW <vladimir.davidovic@thoughtworks.com>
2025-04-13 20:16:06 +0100

    SES1: Skeleton commit

--- base_web_app/globo_web_app/variables.tf
+++ base_web_app/globo_web_app/variables.tf
@@ -10,13 +10,4 @@ variable "prefix" {
   description = "Prefix for resource names"
 } 
 
-variable "access_key" {
-  description = "AWS access key"
-  type        = string
-}
-
-variable "secret_key" {
-  description = "AWS secret key"
-  type        = string
-}
 
